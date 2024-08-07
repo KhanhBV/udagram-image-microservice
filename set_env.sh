54ce538795cff4a7190d7d1491d023e1241cc610
Van Khanh Bui <khanhbvse@gmail.com>
2024-08-07 21:53:14 +0700

    feat Init project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=sa
+export POSTGRES_PASSWORD=Khanhvip74
+export POSTGRES_HOST=database-1.c05hzlzw29a4.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
