7323b0694560f72b68fad3e0793578a79b860b82
Van Khanh Bui <khanhbvse@gmail.com>
2024-08-19 21:32:51 +0700

    update config deployment

--- set_env.sh
+++ set_env.sh
@@ -1,9 +1,9 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=sa
-export POSTGRES_PASSWORD=Khanhvip74
+export POSTGRES_USERNAME=
+export POSTGRES_PASSWORD=
 export POSTGRES_HOST=database-1.c05hzlzw29a4.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
+export POSTGRES_DB=khanhbvdatabase
 export AWS_BUCKET=arn:aws:s3:::mybucket1200798
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
